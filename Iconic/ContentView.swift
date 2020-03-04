ccfbd46764b03e621f955b939625cddcff3c8851
Pedro Almeida <ip4dro@gmail.com>
2020-03-04 22:13:59 +0100

    Create empty project

---
+++ Iconic/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Iconic
+//
+//  Created by Pedro Almeida on 04.03.20.
+//  Copyright © 2020 Pedrofolio.com. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
